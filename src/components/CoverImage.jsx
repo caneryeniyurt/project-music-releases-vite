4d919d3d7802547c38f92e0413a758a4c4499501
Caner <caneryeniyurt@gmail.com>
2023-10-22 20:41:53 +0200

    icons on the images

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,11 +1,29 @@
+import favBtn from "../assets/icons/heart.svg"
+import playBtn from "../assets/icons/play.svg"
+import dotsBtn from "../assets/icons/dots.svg"
+
 export const CoverImage = ({ coverimage }) => {
     return (
-        <div>
+      <div className="CoverImage">
         <img
           src= {coverimage[1].url}
           width={coverimage[1].width}
           height={coverimage[1].height}
         />
+          <div id="overlay">
+            <img src={favBtn}
+            //  id="heart-highlight"
+            className="icons-highlight"></img>
+
+            <img src={playBtn}
+            //  id="play-highlight"
+            className="icons-highlight"></img>
+
+            <img src={dotsBtn}
+            //  id="dots-highlight"
+            className="icons-highlight"></img>
+
+        </div>
       </div>
     );
   };
