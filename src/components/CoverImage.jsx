05428e906456162761a4f7bfa8d2a579dbec80a0
Caner <caneryeniyurt@gmail.com>
2023-10-22 21:32:21 +0200

    icons are added on the images

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -3,32 +3,34 @@ import playBtn from "../assets/icons/play.svg"
 import dotsBtn from "../assets/icons/dots.svg"
 
 export const CoverImage = ({ coverimage }) => {
-    return (
-      <div className="CoverImage">
-        <img
-          src= {coverimage[1].url}
-          width={coverimage[1].width}
-          height={coverimage[1].height}
-        />
-          <div id="overlay">
-            <img src={favBtn}
+  return (
+    <div className="CoverImage">
+      <img
+        src={coverimage[1].url}
+        width={coverimage[1].width}
+        height={coverimage[1].height}
+      />
+      <div id="overlay">
+        <span className="icon-container">
+          <img
+            src={favBtn}
             //  id="heart-highlight"
-            className="icons-highlight"></img>
+            className="heart-icon"
+          ></img>
 
-            <img src={playBtn}
+          <img
+            src={playBtn}
             //  id="play-highlight"
-            className="icons-highlight"></img>
+            className="play-icon"
+          ></img>
 
-            <img src={dotsBtn}
+          <img
+            src={dotsBtn}
             //  id="dots-highlight"
-            className="icons-highlight"></img>
-
-        </div>
+            className="dots-icon"
+          ></img>
+        </span>
       </div>
-    );
-  };
-  
-  Image.defaultProps = {
-    description: "Image is missing",
-  };
-  
+    </div>
+  );
+};
